532e2c3bd18ef5938296cb7a5986537f7ca47ce0
AviralGup7 <f20240294@pilani.bits-pilani.ac.in>
2026-06-13 08:59:47 +0530

    Fix GraphQL module/package collision and resolve test import errors

--- src/analysis/active/graphql/__init__.py
+++ src/analysis/active/graphql/__init__.py
@@ -1,1 +1,17 @@
 """Active analysis GraphQL probes."""
+
+from src.analysis.active.graphql.core import (
+    GRAPHQL_CSRF_ORIGINS,
+    GRAPHQL_DIRECTIVE_BYPASS_QUERIES,
+    _build_get_query_url,
+    _parse_graphql_response,
+    graphql_active_probe,
+)
+
+__all__ = [
+    "GRAPHQL_CSRF_ORIGINS",
+    "GRAPHQL_DIRECTIVE_BYPASS_QUERIES",
+    "_build_get_query_url",
+    "_parse_graphql_response",
+    "graphql_active_probe",
+]
